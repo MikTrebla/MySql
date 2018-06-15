d4ec0b7f6667ae5439d45276de2ea17dbf8838b2
Albert Kim <lisa@Lisas-MacBook-Pro.local>
2018-06-14 21:01:35 -0700

    updated sql file

--- bamazon.sql
+++ bamazon.sql
@@ -9,6 +9,7 @@ CREATE TABLE products(
     stock_quantity INT NOT NULL
 );
 
+
 INSERT INTO products SET
     product_name = 'jeans',
     department_name = 'clothing',
@@ -16,17 +17,21 @@ INSERT INTO products SET
     stock_quantity = 500;
 
 INSERT INTO products SET
-    product_name = 'cast iron pan',
-    department_name = 'cooking',
-    price = 49.99,
-    stock_quantity = 500;
-
-
-SELECT * FROM products;
-
+    product_name = "Casio Men's Digital Watch",
+    department_name = 'accessory',
+    price = 26.99,
+    stock_quantity = 1000;
+    
 INSERT INTO products (product_name, department_name, price, stock_quantity)
 VALUES
 ("Nintendo Switch", "electronics", 299, 1000), ("Canvas Messenger Bag", "accessory", 34.99, 1000),
 ("Echo Dot", "electronics", 39.99, 1000),("Catnip", "pet", 4.65, 1000), 
 ("Cat food", "pet", 14.12, 1000), ("Green Tea", "food", 13.97, 1000), 
-("Pusheen Stuffed Pillow", 'toy' , 16.96, 1000),("Casio Men's Digital Watch", "accessory", 26.99, 1000);
+("Pusheen Stuffed Pillow", 'toy' 16.96, 1000),("Casio Men's Digital Watch", "accessory", 26.99, 1000);
+
+
+
+UPDATE products SET stock_quantity = 3
+WHERE item_id = 1;
+
+SELECT * FROM products;
